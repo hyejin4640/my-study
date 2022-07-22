8383aaaf3751fedab0bd7d2204c223d3fc2b6c58
hyejin4640 <gpwls0498@gmail.com>
2022-07-22 17:18:06 +0900

    delite

--- variabel/10_typeof.js
+++
@@ -1,9 +0,0 @@
-// typeof : 데이터 타입을 확인
-// 값을 타입 문자열로 반환
-
-
-let variabel = Symbol();   //할당된 값에 따라 타입이 결정됨!,동적인언어,위클리타입랭귀지
-console.log(typeof variabel);
-
-// c,java(컴파일언어)
-// int variabel = 0;  <-컴파일언어는 데이터의 타입도 선언을 해줘야함,정적인언어,스트롱랭귀지
