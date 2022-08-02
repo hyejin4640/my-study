8db0536339b089f8121a389b3255945dfa899385
hyejin <105892905+hyejin4640@users.noreply.github.com>
2022-08-03 07:52:48 +0900

    add

---
+++ 웹디실기-8m-2/t-1/script/t-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $(".mainmenu").slideDown("fast");
+});
+
+// $("header nav >ul").mouseenter(function () {
+//   $("ul.lv2").slideDown(800);
+// });
+
+// $("header nav >ul").mouseleave(function () {
+//   $("ul.lv2").stop().slideUp(500);
+// });
