58797f28e3690c442f312c1ef33c7371e9ef63a0
hyejin <105892905+hyejin4640@users.noreply.github.com>
2022-10-17 20:50:08 +0900

    e

--- js-wether-1014-1/js/fetch.js
+++
@@ -1,8 +0,0 @@
-const url =  'https://mdn.github.io/learning-area/javascript/oojs/json/superheroes.json';
-
-fetch(url).then( (response)=>{
-  return response.json();   /*.json 은 json방식으로 들어온string타입을  object로 변환하는 메소드 */
-}).then((data)=>{
-  console.log( data );
-  console.log( typeof data );
-})
